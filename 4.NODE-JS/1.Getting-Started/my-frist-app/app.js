9828ca798dcd3d1b4f50019ecfd526a39ec91832
Ritik Kumar <rk7382687@gmail.com>
2024-04-01 10:31:05 +0530

    first-app

---
+++ 4.NODE-JS/1.Getting-Started/my-frist-app/app.js
@@ -0,0 +1,11 @@
+console.log("Hello World!");
+
+
+// if you run the node js program follow some steps
+/*
+Step 1: - Open terminal
+Step 2:- Go to program directory(with the helf of cmd prompt or use GUIs like vsCode....etc)
+Step 3:- write "node <file-name>" with file extension or without extension but extension is js file not other
+Step 4:- Hit Enter
+*/ 
+// Output is "Hello World!"
