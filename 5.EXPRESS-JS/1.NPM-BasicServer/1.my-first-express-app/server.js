e59184dde70a1b38ba1f81953b0849ebdfd3e2f5
Ritik Kumar <rk7382687@gmail.com>
2024-04-05 10:29:42 +0530

    first express app

---
+++ 5.EXPRESS-JS/1.NPM-BasicServer/1.my-first-express-app/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+// instance
+const app = express();
+
+// Create the PORT
+const PORT = 8082;
+
+// Define the router handler
+app.get("/", (req, res) => {
+    res.send("Hello world");
+})
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+})
