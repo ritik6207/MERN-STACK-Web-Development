b855c180ae3968e14cb3de4d2fa414449ac02b06
Ritik Kumar <rk7382687@gmail.com>
2024-04-06 09:51:00 +0530

    basic router

---
+++ 5.EXPRESS-JS/2.Routing/1.ExpressRouter/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const userRouter = require("./routes/usersRouter");
+const postRouter = require("./routes/postRouter");
+const app = express();
+const PORT = 8082;
+// Home Route
+app.get("/", (req, res) => {
+    res.json({
+        message: "Welcome to the main app",
+    })
+})
+//! ------USERS ROUTE------
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
+
+// start the server
+app.listen(PORT, console.log(`Server is running on port ${PORT}`))
