b171532c25702d3a5e94eb1e4c362dff8b5450e4
Ritik Kumar <rk7382687@gmail.com>
2024-05-02 14:36:13 +0530

    thirdPartyCss

---
+++ 10.ReactJS/4.styling-third-party.jsx/src/App.js
@@ -0,0 +1,36 @@
+const App = () => {
+  return (
+    // Herea we use third pary t Telwind Css for styling
+    // <h1 className="text-3xl text-red-900 font-bold underline">Hello world!</h1>
+
+    // Here we use third party bootstarp css for styling
+    <div>
+      <div className="alert alert-primary" role="alert">
+        A simple primary alert—check it out!
+      </div>
+      <div className="alert alert-secondary" role="alert">
+        A simple secondary alert—check it out!
+      </div>
+      <div className="alert alert-success" role="alert">
+        A simple success alert—check it out!
+      </div>
+      <div className="alert alert-danger" role="alert">
+        A simple danger alert—check it out!
+      </div>
+      <div className="alert alert-warning" role="alert">
+        A simple warning alert—check it out!
+      </div>
+      <div className="alert alert-info" role="alert">
+        A simple info alert—check it out!
+      </div>
+      <div className="alert alert-light" role="alert">
+        A simple light alert—check it out!
+      </div>
+      <div className="alert alert-dark" role="alert">
+        A simple dark alert—check it out!
+      </div>
+    </div>
+  );
+};
+
+export default App;
