672c33040bffd76bac4adf83da16876a75d4c284
Ritik Kumar <rk7382687@gmail.com>
2024-05-02 15:05:55 +0530

    conponents

---
+++ 10.ReactJS/5.components.jsx/src/App.js
@@ -0,0 +1,15 @@
+import Userprofile from "./components/UserProfile";
+const App = () => {
+  return (
+    <div>
+      {/* Call/render the components */}
+      <Userprofile />
+      <Userprofile />
+      <Userprofile />
+      <Userprofile />
+      <Userprofile />
+    </div>
+  );
+};
+
+export default App;
---
+++ 10.ReactJS/5.components.jsx/src/components/UserProfile.js
@@ -0,0 +1,24 @@
+const Userprofile = () => {
+  return (
+    <div style={{ border: "1px solid #ccc", padding: "20px", margin: "20px" }}>
+      <h2>User Profile</h2>
+      <p>
+        <b>Name</b>: Ritik kumar
+      </p>
+      <p>
+        <b>Email</b>: ritik@gmail.com
+      </p>
+      <p>
+        <b>Location</b>: India
+      </p>
+      <p>
+        <b>About</b>: Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Laboriosam consequuntur placeat, natus quaerat maiores iure eius unde
+        repudiandae pariatur facere accusamus ex, alias nam et adipisci sunt
+        quae quisquam ipsam.
+      </p>
+    </div>
+  );
+};
+
+export default Userprofile;
