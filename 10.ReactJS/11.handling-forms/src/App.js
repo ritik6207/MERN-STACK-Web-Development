d93523b52a8b56c7410f80724fbd78531abd1754
Ritik Kumar <rk7382687@gmail.com>
2024-05-05 17:24:03 +0530

    handlingForms

---
+++ 10.ReactJS/11.handling-forms/src/App.js
@@ -0,0 +1,11 @@
+import StudentRegistrationForm from "./Components/StudentRegistrationForm";
+
+function App() {
+  return (
+    <div>
+      <StudentRegistrationForm/>
+    </div>
+  );
+}
+
+export default App;
